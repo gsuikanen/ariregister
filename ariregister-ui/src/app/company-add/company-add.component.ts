d95ac80c2b6b4eb3f52ebdeeb8d0afbe6fcb8b90
gsuikanen <georgi.suikanen@gmail.com>
2022-10-17 18:48:02 +0300

    added totalCapital calculation

--- ariregister-ui/src/app/company-add/company-add.component.ts
+++ ariregister-ui/src/app/company-add/company-add.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, FormArray, Validators } from '@angular/forms';
-import { AriregisterApiService } from '../services/ariregister-api.service'; 
+import { AriregisterApiService } from '../services/ariregister-api.service';
 import { faWarning, faCheckCircle } from '@fortawesome/free-solid-svg-icons';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-company-add',
@@ -14,18 +15,27 @@ export class CompanyAddComponent implements OnInit {
   apiResponse: any;
   warningIcon = faWarning;
   successIcon = faCheckCircle;
+  totalCapital = 0;
+  subscription: Subscription;
 
-  constructor(private fb: FormBuilder, 
+  constructor(private fb: FormBuilder,
     private Api: AriregisterApiService) {
+    
+  }
+
+  ngOnInit(): void {
     this.AddCompanyForm = this.fb.group({
       companyName: ['', Validators.required],
       companyCode: ['', [Validators.required, Validators.pattern('^.{7}$')]],
       companyRegdt: new Date().toISOString().split('T')[0],
+      companyCapital: this.totalCapital,
       owners: this.fb.array([])
     });
-  }
 
-  ngOnInit(): void { }
+    this.subscription = this.owners.valueChanges.subscribe(data => {
+      this.totalCapital = data.reduce((a,b) => a + +b.ownerCapital, 0)
+    })
+   }
 
   get owners(): FormArray {
     return this.AddCompanyForm.get('owners') as FormArray;
@@ -45,13 +55,13 @@ export class CompanyAddComponent implements OnInit {
       ownerSurname: '',
       ownerType: 'priv',
       ownerCode: ['', Validators.required],
-      ownerCapital: [0, [Validators.required, Validators.min(1)]] ,
+      ownerCapital: [0, [Validators.required, Validators.min(1)]],
     })
- }
+  }
 
- addOwner() {
-  this.owners.push(this.newOwner());
-}
+  addOwner() {
+    this.owners.push(this.newOwner());
+  }
 
   deleteOwner(i: number) {
     this.owners.removeAt(i);
@@ -59,6 +69,15 @@ export class CompanyAddComponent implements OnInit {
 
   getOwnerType(i: number): string {
     return this.owners.value.at(i).ownerType
+  }
+
+  getTotalCapital(): number {
+    let cap = 0;
+    //let len = this.owners.value.len();
+    // for (let i = 0; i <= len; i++) {
+    //   cap += this.owners.value.at(i).ownerCapital;
+    // }
+    return cap;
   }
 
   onSubmit() {
